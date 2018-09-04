4a65e4c8110799f6b9e2cbcb6b087228ffe1a2a3
Luis Emilio Juarez <luis_juarez2394@hotmail.com>
2018-09-03 23:37:59 -0600

    segundo efecto de estilo agregado

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,32 @@
 
 
-function changeFont(){
-    document.getElementById("articleinit").style.fontFamily = "Impact,Charcoal,sans-serif"; 
+function changeFont()
+{
+    if (String(document.getElementById("page-top").style.fontFamily) == "Impact, Charcoal, sans-serif") 
+    {
+        document.getElementById("page-top").style.fontFamily = "Arial,Helvetica,sans-serif";
+    } 
+    else
+    {
+        document.getElementById("page-top").style.fontFamily = "Impact,Charcoal,sans-serif";
+    }
 }
+
+function changeBackColor()
+{
+    if (String(document.body.style.background) == "red none repeat scroll 0% 0%") 
+    {
+        document.body.style.background = "grey";
+    } 
+    else
+    {
+        document.body.style.background = "red";
+    }
+}
+
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar-fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
